34292844d0c533d5d102aabd1a152d501878e252
Sebastián Pacheco <44266708+Santasy@users.noreply.github.com>
2019-07-31 15:51:09 -0400

    Añadido codigo base para el programa
    - Código source de la página hasta el momento
    - Notas con bibliografía y apuntes generales del equipo.

---
+++ Codigo/src/Usuario.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Usuario extends React.Component{
+	constructor(props){
+		super(props);
+		this.nombre = "Usuario1";
+		this.id_malla = "Malla1";
+		this.sem_actual = 1;
+	}
+	render(){
+		return;
+	}
+};
